b52bb4a1f3f981a7fa0fbba314e0ec49e9b599e3
santosh <santosh26192@gmail.com>
2023-10-14 22:59:43 -0500

    Adding models.py and other db related files
    Adding models.py and other db related files

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Shuttle, Ticket, ShuttleSchedule
 # Register your models here.
+
+admin.site.register(Shuttle)
+admin.site.register(Ticket)
+admin.site.register(ShuttleSchedule)
+
