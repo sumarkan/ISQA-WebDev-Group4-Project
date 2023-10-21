62f3e5030befb13f74cc7a77993d65df983b1318
santosh <santosh26192@gmail.com>
2023-10-21 18:36:26 -0500

    New model
    New model

--- ticketing/admin.py
+++ ticketing/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Shuttle, Ticket, ShuttleSchedule
+from .models import Shuttle, Ticket, ShuttleSchedule, PaymentDetails
 # Register your models here.
 
 admin.site.register(Shuttle)
 admin.site.register(Ticket)
 admin.site.register(ShuttleSchedule)
+admin.site.register(PaymentDetails)
 
