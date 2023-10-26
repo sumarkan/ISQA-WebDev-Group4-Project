95035f2afc857875cf15c4eb60c775747f4bace0
UNO-WEBDEV-GROUP4 <146895210+UNO-WEBDEV-GROUP4@users.noreply.github.com>
2023-10-25 20:00:30 -0500

    Update urls.py

--- shuttle_ticketing/urls.py
+++ shuttle_ticketing/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
+                                                 settings.MEDIA_ROOT}),  # serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
+                                                  settings.STATIC_ROOT}),  # serve static files when deployed
     path('', include('ticketing.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
