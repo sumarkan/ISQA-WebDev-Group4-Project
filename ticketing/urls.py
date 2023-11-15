df2000826410eff2527c96acd810936686a6e9a7
jilltorkelson <JillT8585@gmail.com>
2023-11-14 18:15:30 -0600

    Sprint2

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -15,9 +15,5 @@ urlpatterns = [
     path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-   # path('accounts/', include('django.contrib.auth.urls')), # COPIED FROM TUTORIAL
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'), # COPIED FROM TUTORIAL
 ]
-    # IMPORTANT – THE URL BELOW MUST BE AFTER THE PASSWORD RESET CUSTOM FORM ABOVE if included- was in front of the reset password
-            # bringing up the django version first
- # path('', include('django.contrib.auth.urls')),
