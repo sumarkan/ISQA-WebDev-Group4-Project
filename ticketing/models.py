aa6ad4a1ba04c432e5ef72201b555e0adecb1e35
Apollin7 <ndziemariefrancine@gmail.com>
2023-12-11 00:04:45 -0600

    Files changes

--- ticketing/models.py
+++ ticketing/models.py
@@ -2,10 +2,9 @@ import uuid
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-from datetime import date
+from datetime import date, datetime, timedelta
 
 
-# Create your models here.
 class Shuttle(models.Model):
     """Model representing an Shuttle."""
     shuttle_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
@@ -28,7 +27,7 @@ class Shuttle(models.Model):
 
 
 class ShuttleSchedule(models.Model):
-    """Model representing an Shuttle_schedule."""
+    """Model representing a Shuttle_schedule."""
     shuttle_schd_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
                                      help_text='Unique ID for this particular shuttle schedule')
     schd_time = models.TimeField()
@@ -48,44 +47,39 @@ class ShuttleSchedule(models.Model):
 
 
 class Ticket(models.Model):
-    """Model representing an Shuttle_schedule."""
-    # // customer_id, purchased_date, payment_status, shuttle_schd_id
+    """Model representing a Shuttle schedule."""
     ticket_number = models.UUIDField(primary_key=True, default=uuid.uuid4,
                                      help_text='Unique ID for this particular ticket purchased')
     purchased_date = models.DateField()
     shuttle_schd_id = models.ForeignKey('ShuttleSchedule', on_delete=models.RESTRICT, null=True)
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
-
     class Meta:
         ordering = ['purchased_date']
 
     def get_absolute_url(self):
-        """Returns the URL to access a particular shuttle schedule instance."""
-        return reverse('Ticket', args=[str(self.ticket_number)])
+        """Returns the URL to access a particular ticket instance."""
+        return reverse('ticket_detail', args=[str(self.ticket_number)])
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.ticket_number}'
 
     def is_past(self):
-        """Boolean to determine if ticket is before today"""
-        return bool(self.shuttle_schd_id.schd_date and date.today() < self.shuttle_schd_id.schd_date)
+        """Determines if ticket date is before today."""
+        return self.shuttle_schd_id.schd_date < date.today()
 
     def is_today(self):
-        """Boolean to determine if ticket is today"""
-        return bool(self.shuttle_schd_id.schd_date == date.today())
+        """Determines if ticket date is today."""
+        return self.shuttle_schd_id.schd_date == date.today()
 
     def refundable(self):
-        """Boolean to determine if ticket is still refundable (more than 24 hours in advance)"""
-# THIS IS WRONG CALC - NEEDS TO DETERMINE IF MORE THAN 1 DAY
-        return bool(self.shuttle_schd_id.schd_date and date.today() < self.shuttle_schd_id.schd_date)
-
+        """Determines if ticket is still refundable (more than 24 hours in advance)."""
+        return datetime.combine(self.shuttle_schd_id.schd_date, self.shuttle_schd_id.schd_time) - datetime.now() > timedelta(days=1)
 
 class PaymentDetails(models.Model):
-    """Model representing an PaymentStatus."""
+    """Model representing Payment Details."""
 
-# look for loan status 
     PAID = 'PAID'
     CANCELLED = 'CANCELLED'
     PENDING = 'PENDING'
@@ -94,27 +88,19 @@ class PaymentDetails(models.Model):
         (PAID, 'Paid'),
         (CANCELLED, 'Cancelled'),
         (PENDING, 'Pending'),
-        (NO_STATUS, 'No_status'),
-    )
-    payment_status = models.CharField(
-        max_length=50,
-        choices=PAYMENT_STATUS_OPTIONS,
-        default=NO_STATUS,
+        (NO_STATUS, 'No Status'),
     )
 
     transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4,
                                       help_text='Unique ID for this particular transaction')
+    payment_date = models.DateField(null=True, blank=True)  # Added payment date
+    payment_status = models.CharField(max_length=50, choices=PAYMENT_STATUS_OPTIONS, default=NO_STATUS)
     payment_method = models.CharField(max_length=100)
-    amount = models.IntegerField()
-    ticket = models.ForeignKey(Ticket, on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Changed to DecimalField for monetary values
+    ticket = models.ForeignKey('Ticket', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
-        ordering = ['payment_status', 'payment_method', 'amount']
-
-    def get_absolute_url(self):
-        """Returns the URL to access a particular shuttle instance."""
-        return reverse('shuttle_detail', args=[str(self.transaction_id)])
+        ordering = ['payment_date', 'payment_status', 'amount']
 
     def __str__(self):
-        """String for representing the Model object."""
-        return f'{self.transaction_id}'
+        return f'Transaction ID: {self.transaction_id}'
--- ticketing/views.py
+++ ticketing/views.py
@@ -4,65 +4,129 @@ from django.views import generic
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib import messages
+from django.urls import reverse_lazy
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 
 
-def index(request):
-    return render(request, 'ticketing/index.html')
-
-
 def home(request):
-    return render(request, 'ticketing/base.html')
+    context = {'show_back_button': False}
+    return render(request, 'ticketing/base.html', context)
 
 
-#class for Shuttle Lists
-class ShuttleListView(generic.ListView):
-    model = Shuttle
+# Index view
+def index(request):
+    context = {'show_back_button': False}
+    return render(request, 'ticketing/index.html', context)
 
 
+# Shuttle list view
 def shuttle_list(request):
     list_shuttles = Shuttle.objects.all()
-    return render(request, 'shuttle_list.html', {'shuttle_list': list_shuttles})
+    context = {
+        'shuttle_list': list_shuttles,
+        'show_back_button': True
+    }
+    return render(request, 'shuttle_list.html', context)
 
 
-# class for ticket lists
-class TicketListView(generic.ListView):
-    model = Ticket
+class ShuttleCreate(CreateView):
+    model = Shuttle
+    fields = ['name', 'capacity', 'color', 'operated_by']
+    template_name = 'shuttle_create.html'
 
 
+class ShuttleUpdate(UpdateView):
+    model = Shuttle
+    fields = ['name', 'capacity', 'color', 'operated_by']
+    template_name = 'shuttle_update.html'  #
+
+    def get_success_url(self):
+        """
+        After successfully updating the shuttle, redirect to the shuttle list.
+        """
+        return reverse_lazy('shuttle_list')
+
+class ShuttleDelete(DeleteView):
+    model = Shuttle
+    template_name = 'shuttle_confirm_delete.html'  # Specify your template name here
+    success_url = reverse_lazy('shuttle_list')  # Redirect to shuttle list after deleting
+
+    def get_context_data(self, **kwargs):
+        """
+        Provide additional context to the template, if needed.
+        """
+        context = super(ShuttleDelete, self).get_context_data(**kwargs)
+        return context
+
+
+# Ticket list view
 def ticket_list(request):
     list_tickets = Ticket.objects.all()
-    return render(request, 'ticket_list.html', {'ticket_list': list_tickets})
+    context = {
+        'ticket_list': list_tickets,
+        'show_back_button': True
+    }
+    return render(request, 'ticket_list.html', context)
 
 
-class ScheduleListView(generic.ListView):
-    model = ShuttleSchedule
-
-
+# Schedule list view
 def schedule_list(request):
     lists_schd_shuttles = ShuttleSchedule.objects.all()
-    return render(request, 'schedule_list.html', {'schedule_list': lists_schd_shuttles})
+    context = {
+        'schedule_list': lists_schd_shuttles,
+        'show_back_button': True
+    }
+    return render(request, 'schedule_list.html', context)
 
-
-def privacy_policy(request):
-    return render(request, 'privacy_policy.html')
-
-
-def terms_of_service(request):
-    return render(request, 'terms_of_service.html')
-
-
-def contact_us(request):
-    return render(request, 'contact_us.html')
-
+def my_ticket_number(request):
+    return render(request, 'my_ticket_number.html')
 
 def payment_details_list(request):
+    if request.method == 'POST':
+        ticket_number = request.POST.get('ticket_number')
+        try:
+            ticket = Ticket.objects.get(ticket_number=ticket_number)
+            payment_details = PaymentDetails.objects.filter(ticket=ticket)
+            return render(request, 'payment_details_list.html', {'payment_details_list': payment_details, 'show_back_button': True})
+        except Ticket.DoesNotExist:
+            # Handle the case where the ticket does not exist
+            return render(request, 'my_ticket_number.html', {'error': 'Ticket not found'})
+    else:
+        return redirect('my_ticket_number')
+
+
+# Payment details list view
+def payment_details_list(request):
     payments = PaymentDetails.objects.all()
-    context = {'payments': payments}
+    context = {
+        'payments': payments,
+        'show_back_button': True
+    }
     return render(request, 'payment_details_list.html', context)
+def payment_detail_view(request, payment_id):
+    payment = get_object_or_404(payment_details_list, pk=payment_id, user=request.user)
+    return render(request, 'payment_detail.html', {'payment': payment,  'show_back_button': True})
+
+
+# Privacy policy view
+def privacy_policy(request):
+    context = {'show_back_button': True}
+    return render(request, 'privacy_policy.html', context)
+
+
+# Terms of service view
+def terms_of_service(request):
+    context = {'show_back_button': True}
+    return render(request, 'terms_of_service.html', context)
+
+
+# Contact us view
+def contact_us(request):
+    context = {'show_back_button': True}
+    return render(request, 'contact_us.html', context)
 
 
 class ShuttleCreate(CreateView):
@@ -118,6 +182,7 @@ class MyTickets(LoginRequiredMixin,generic.ListView):
         return Ticket.objects.filter\
             (customer=self.request.user).order_by('purchased_date')
 
+
 class PasswordResetView(generic.ListView):
     """Generic class-based view listing tickets purchased by the customer logged in"""
     model = Ticket #UNSURE ABOUT THIS ************
@@ -127,6 +192,7 @@ class PasswordResetView(generic.ListView):
     def get_queryset(self):
         return Ticket.objects.filter\
             (customer=self.request.user).order_by('purchased_date')
+
 
 class PasswordResetDoneView(generic.ListView):
     """Generic class-based view listing tickets purchased by the customer logged in"""
@@ -138,6 +204,7 @@ class PasswordResetDoneView(generic.ListView):
         return Ticket.objects.filter\
             (customer=self.request.user).order_by('purchased_date')
 
+
 class PasswordResetConfirmView(generic.ListView):
     """Generic class-based view listing tickets purchased by the customer logged in"""
     model = Ticket #UNSURE ABOUT THIS ************
@@ -146,7 +213,8 @@ class PasswordResetConfirmView(generic.ListView):
 
     def get_queryset(self):
         return Ticket.objects.filter\
-            (customer=self.request.user).order_by('purchased_date')
+           (customer=self.request.user).order_by('purchased_date')
+
 
 class PasswordResetCompleteView(generic.ListView):
     """Generic class-based view listing tickets purchased by the customer logged in"""
@@ -157,3 +225,35 @@ class PasswordResetCompleteView(generic.ListView):
     def get_queryset(self):
         return Ticket.objects.filter\
             (customer=self.request.user).order_by('purchased_date')
+
+
+class ShuttleScheduleCreate(CreateView):
+    model = ShuttleSchedule
+    fields = ['schd_time', 'schd_date', 'shuttle_id']
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.save()
+        return HttpResponseRedirect(reverse('schedule_list'))
+
+
+class ShuttleScheduleUpdate(UpdateView):
+    model = ShuttleSchedule
+    fields = ['schd_time', 'schd_date', 'shuttle_id']
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.save()
+        return HttpResponseRedirect(reverse('schedule_list'))
+
+
+def shuttleschedule_delete(request, pk):
+    schedule = get_object_or_404(ShuttleSchedule, pk=pk)
+    name = schedule.shuttle_id.name
+    try:
+        schedule.delete()
+        messages.success(request, ('schedule for shuttle with '+name + " has been deleted"))
+    except:
+        messages.success(request, ('schedule for shuttle with '+name + ' cannot be deleted, Shuttle does not exists'))
+
+    return redirect('schedule_list')
